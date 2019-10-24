aed759b641f619230a341b2c261949ce989e159c
Tyson Corvin <tyson.corvin@gmail.com>
2019-10-23 20:27:06 -0400

    Add project files.

---
+++ sandbox.synuit.io/Controllers/IpifyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ipify.Net.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ipify")]
+    public class IpifyController : Controller
+    {
+ 
+        // GET: api/Ipify/getAddr
+        [HttpGet("{id}", Name = "Get")]
+        public string GetAddress()
+        {
+         var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress;
+         return remoteIpAddress.ToString();
+        }
+        
+       
+        
+   
+    }
+}
---
+++ sandbox.synuit.io/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sandbox.synuit.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
